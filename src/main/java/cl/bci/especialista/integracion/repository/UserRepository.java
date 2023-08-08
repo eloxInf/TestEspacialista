c6ae0b7657f3c8eea4c2a12fde4d8f8149ffc635
Angelo Venegas <avenegas@emeal.nttdata.com>
2023-08-08 10:38:29 -0400

    change visibility

--- src/main/java/cl/bci/especialista/integracion/repository/UserRepository.java
+++ src/main/java/cl/bci/especialista/integracion/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package cl.bci.especialista.integracion.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import cl.bci.especialista.integracion.entity.UsersEntity;
@@ -15,7 +18,13 @@ public interface UserRepository extends JpaRepository<UsersEntity, String> {
 	Boolean existsByEmailAndPass(String email, String pass);
 	
 	Boolean existsByidUser(String idUser);
-		
+	
+	
+    @Query(value ="SELECT "
+    		+ "up "
+    		+ "FROM UsersEntity up "
+    		+ "")
+	List<UsersEntity> andPhoneMixed();
 	
 	
 	
