b15828c0c387f40c5ac1370daad18bfb30600622
Angelo Venegas <avenegas@emeal.nttdata.com>
2023-08-15 23:16:43 -0400

    Implementando seguridad basica.

--- src/main/java/cl/rest/especialista/integracion/conf/SecurityConfig.java
+++ src/main/java/cl/rest/especialista/integracion/conf/SecurityConfig.java
@@ -1,28 +1,64 @@
 package cl.rest.especialista.integracion.conf;
 
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+import javax.security.auth.message.config.AuthConfig;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+	/*
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 		
+		//return httpSecurity.build();
+	
 		return httpSecurity
 				.csrf().disable() // Activarlo en caso de usarlo con formularios con personas.
 				.authorizeHttpRequests()
-					.antMatchers(HttpMethod.POST, "/security/v1/loginUser").permitAll()
-					.antMatchers(HttpMethod.POST, "/especialista/v1/users").permitAll()
+					.antMatchers(HttpMethod.POST, "/especialista/v1/user").permitAll()
+					.antMatchers(HttpMethod.GET, "/security/v1/loginUser").permitAll()
 					.anyRequest().authenticated()
 				.and()
 				.formLogin()
 				.and()
-				.build();		
+				.build();
+				
+	}*/
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		
+		//return httpSecurity.build();
+	
+		return httpSecurity
+				.csrf().disable() // Activarlo en caso de usarlo con formularios con personas.
+				.authorizeHttpRequests( auth ->{
+					auth.antMatchers(HttpMethod.POST, "/especialista/v1/user").permitAll();
+					auth.antMatchers(HttpMethod.GET, "/security/v1/loginUser").permitAll();
+					auth.anyRequest().authenticated();
+				})
+				.formLogin()
+					.successHandler(successHandeler())
+				.and()
+				.build();
+		
+				
+	}
+	
+	
+	public AuthenticationSuccessHandler successHandeler() {
+		return ((request, response, authentication) -> {
+			response.sendRedirect("/swagger-ui.html");
+		});
+				
 	}
 }
